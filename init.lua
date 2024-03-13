59e86464b18cce8fb717109e55f2cb35ee53cdfc
AlejandroFNadal <alexandronadal@gmail.com>
2024-03-13 08:35:06 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- 🌑init.lua
+
+require("settings")
+require("plugins")
+require("maps")
+
+-- colorscheme config: kanagawa
+-- local themeStatus, kanagawa = pcall(require, "kanagawa")
+
+vim.cmd("colorscheme morning")
+-- if themeStatus then
+-- else
+-- return
+--end
+
