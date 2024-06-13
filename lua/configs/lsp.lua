8d811cc63b43c0f2369f1d814303408921daa69f
Alejandro Nadal <alexandronadal@gmail.com>
2024-06-13 08:15:29 +0200

    lsp was not working in js

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -14,7 +14,7 @@ local on_attach = function(client, bufnr)
             group = vim.api.nvim_create_augroup("Format", { clear = true }),
             buffer = bufnr,
             callback = function()
-                vim.lsp.buf.formatting_seq_sync()
+                vim.lsp.buf.format({ bufnr = bufnr })
             end,
         })
     end
